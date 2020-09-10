23071f0f7413ed23c10308d9b52a47450968caea
Yaroslav Shumlianskyi <yshumlianskyi@intellias.com>
2020-09-10 18:30:25 +0300

    Specify main font

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminess Powerline:size=14", "monospace:size=10" };
-static const char dmenufont[]       = "Terminess Powerline:size=14";
+static const char *fonts[]          = { "xos4 Terminus:size=18:antialias=false:autohint=true:fixed=true" };
+static const char dmenufont[]       = "xos4 Terminus:size=18:antialias=false:autohint=true:fixed=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -62,8 +62,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *lockscrn[] = { "i3lock-fancy", NULL };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
